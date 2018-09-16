b292c9e7e154bfea2cf0ab2242658f31757c3e70
Ehsan <ehsan@integrate.ai>
2018-09-15 20:38:31 -0400

    Adding the query.py script

---
+++ query.py
@@ -0,0 +1,17 @@
+import pickle #cPickle if Python 2.7
+import os
+glove_dir = r"the path where you downloaded the pickled dictionaries to"+"\\"
+file_names = os.listdir(glove_dir)
+word = "example" # the word being queried
+embedding_vector = []
+for f_name in file_names:
+    with open(glove_dir + f_name, "rb") as file:
+        gdict = pickle.load(file)
+        if(word in gdict):
+            embedding_vector = gdict[word]
+            break
+
+if(len(embedding_vector == 0)):
+	print("Queried word not found!")
+else:
+	print("Queried vector:", str(embedding_vector))
